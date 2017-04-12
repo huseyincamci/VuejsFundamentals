5040bb8e3ce6cd00113045651b8c9047520c73f2
huseyincamci <hcamci.ce@gmail.com>
2017-04-12 19:20:38 +0300

    Directives Par 3

---
+++ Part3/app.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Vue Js',
+        intro: 'Welcome to the Tutorial <small>It is all about Vue.js</small>',
+        viewed: true
+    }
+})
