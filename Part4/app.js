8b8554a461bb1563ee57c3a1d0c552bf006b4cc0
huseyincamci <hcamci.ce@gmail.com>
2017-04-18 12:03:56 +0300

    Using V-Bind Directive

---
+++ Part4/app.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'hello world',
+        title: 'You loaded the page on ' + new Date(),
+        url: 'https://vuejs.org/images/logo.png',
+        imgTitle: 'Vue js Logo'
+    }
+})
