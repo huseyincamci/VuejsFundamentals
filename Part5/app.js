5f7964f88f56eed38790acbcb90e0aaea82d0bde
huseyincamci <hcamci.ce@gmail.com>
2017-04-18 12:16:40 +0300

    Looping with V-For Directive

---
+++ Part5/app.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue World',
+        todos: [
+            { text: 'Learn Vue', id: 1 },
+            { text: 'Like the video', id: 2 },
+            { text: 'Subscribe to the channel', id: 3 }
+        ]
+    }
+})
